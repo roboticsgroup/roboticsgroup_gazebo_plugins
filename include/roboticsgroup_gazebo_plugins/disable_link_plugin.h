da58a64408e6967433228a8e366185d380a64c82
Konstantinos Chatzilygeroudis <costashatz@gmail.com>
2017-03-07 13:11:49 +0100

    New formatting

--- include/roboticsgroup_gazebo_plugins/disable_link_plugin.h
+++ include/roboticsgroup_gazebo_plugins/disable_link_plugin.h
@@ -6,17 +6,17 @@ Redistribution and use in source and binary forms, with or without modification,
 
 1. Redistributions of source code must retain the above copyright notice, this list of conditions and the following disclaimer.
 
-2. Redistributions in binary form must reproduce the above copyright notice, this list of conditions and the following disclaimer 
+2. Redistributions in binary form must reproduce the above copyright notice, this list of conditions and the following disclaimer
     in the documentation and/or other materials provided with the distribution.
 
-3. Neither the name of the copyright holder nor the names of its contributors may be used to endorse or promote products derived 
+3. Neither the name of the copyright holder nor the names of its contributors may be used to endorse or promote products derived
     from this software without specific prior written permission.
 
-THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, 
-BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT 
-SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL 
-DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS 
-INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE 
+THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING,
+BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT
+SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE
 OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 **/
 
@@ -35,33 +35,30 @@ OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISE
 #include <gazebo/physics/physics.hh>
 #include <gazebo/common/common.hh>
 
-namespace gazebo
-{
-  class DisableLinkPlugin : public ModelPlugin
-  {
+namespace gazebo {
+    class DisableLinkPlugin : public ModelPlugin {
     public:
-      DisableLinkPlugin();
-      ~DisableLinkPlugin();
+        DisableLinkPlugin();
+        ~DisableLinkPlugin();
 
-      void Load(physics::ModelPtr _parent, sdf::ElementPtr _sdf);
-      void UpdateChild();
+        void Load(physics::ModelPtr _parent, sdf::ElementPtr _sdf);
+        void UpdateChild();
 
     private:
-      // Parameters
-      std::string link_name_;
+        // Parameters
+        std::string link_name_;
 
-      bool kill_sim;
+        bool kill_sim;
 
-      // Pointers to the joints
-      physics::LinkPtr link_;
+        // Pointers to the joints
+        physics::LinkPtr link_;
 
-      // Pointer to the model
-      physics::ModelPtr model_;
+        // Pointer to the model
+        physics::ModelPtr model_;
 
-      // Pointer to the world
-      physics::WorldPtr world_;
-
-  };
+        // Pointer to the world
+        physics::WorldPtr world_;
+    };
 }
 
 #endif
